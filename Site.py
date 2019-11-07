d9b17a93b0248baac5f9a265fb68fc3f005d402f
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-07 20:38:14 -0200

    Modulador de rotas

--- Site.py
+++ Site.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
-from Forms import MensagemgForm
-from flask_sqlalchemy import SQLALchemy
+from Forms import MensagemForm
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///msgs.db'
-app.config['SECRET_KEY'] = ""
+app.config['SECRET_KEY'] = "vbhfdbvhbfdvbfdbvfd"
 db = SQLAlchemy(app)
 
-class Msg(db.Model):
+class Mensagem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    msg = db.Column(db.Text, nullable=False)
+    mensagem = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        return '<MSG: %r>' % self.msg
+        return '<MSG: %r>' % self.mensagem
 
         
 @app.route("/")
 def Index():
-    msgs = Mensagem.query.all()
+    msgs = Mensagem.query.filter_by(mensagem=mensagem).first()
     return render_template("Index.html", msgs=msgs)
 
 @app.route("/formulario", methods=['POST', 'GET'])
 def Form():
     form = MensagemForm()
     if form.validate_on_submit():
-        msg1 = MensagemForm()
-        msg1.mensagem = form.mensagem.data
-        db.session.add()
+        msg = Mensagem()
+        msg.mensagem = form.mensagem.data
+        db.session.add(msg)
         db.session.commit()
 
     return render_template("Formulario.html", form=form)
