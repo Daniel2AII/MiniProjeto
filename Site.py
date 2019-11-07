a45de3ce231a6df9063d8924b01e38d832ef5409
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-07 06:27:14 -0200

    Modulador do projeto

---
+++ Site.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/")
+def Index():
+    return render_template("Index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
