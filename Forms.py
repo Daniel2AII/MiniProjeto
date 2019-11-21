459b6b7c45f380a89231d818b05ebdbdc25f701e
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-21 08:06:40 -0200

    Modulador de formularios

--- Forms.py
+++ Forms.py
@@ -1,7 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email
+
+class CadastroUsuario(FlaskForm):
+    login = StringField('Login', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired()])
+    button = SubmitField('Fazer Cadastro')
 
 class MensagemForm(FlaskForm):
     mensagem = StringField('Mensagem', validators=[DataRequired()])
+    titulo = StringField('Título', validators=[DataRequired()])
     button = SubmitField('Enviar MSG')
