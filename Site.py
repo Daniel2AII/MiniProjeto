3790030f5874c7ccc860775474f54bbcae6cc48e
Daniel Alves Cruz <dalvescruz3002@gmail.com>
2019-11-21 08:06:57 -0200

    Modulador de rotas e banco de dados

--- Site.py
+++ Site.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from Forms import MensagemForm
+from Forms import MensagemForm, CadastroUsuario
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,30 +7,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///msgs.db'
 app.config['SECRET_KEY'] = "vbhfdbvhbfdvbfdbvfd"
 db = SQLAlchemy(app)
 
+class Usuario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(10), unique=False, nullable=False)
+
+    def __repre__(self):
+        return '[User: %r]' % self.login    
+
 class Mensagem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     mensagem = db.Column(db.Text, nullable=False)
+    titulo = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):
-        return '<MSG: %r>' % self.mensagem
+    def __repre__(self):
+        return self.mensagem
 
         
 @app.route("/")
 def Index():
-    msgs = Mensagem.query.filter_by(mensagem=mensagem).first()
-    return render_template("Index.html", msgs=msgs)
+    return render_template("Index.html")
 
-@app.route("/formulario", methods=['POST', 'GET'])
+@app.route("/cadastro", methods=['POST', 'GET'])
 def Form():
-    form = MensagemForm()
+
+    form = CadastroUsuario()
+    print(form.login.data)
+    print(form.email.data)
+    print(form.senha.data)
+
     if form.validate_on_submit():
-        msg = Mensagem()
-        msg.mensagem = form.mensagem.data
-        db.session.add(msg)
+        user1 = Usuario()
+        user1.login = form.login.data
+        user1.email = form.email.data
+        user1.senha = form.senha.data
+        db.session.add(user1)
         db.session.commit()
 
-    return render_template("Formulario.html", form=form)
-
+    return render_template("Cadastro.html", form=form)
 
 if __name__ == "__main__":
     app.run(debug = True)
